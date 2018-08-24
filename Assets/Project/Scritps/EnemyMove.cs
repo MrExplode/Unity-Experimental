49f8762d20ae29890beb6afdbc5d005dc2813249
MrExplode <pjanos@outlook.hu>
2018-08-24 17:21:28 +0200

    Backup added

---
+++ Assets/Project/Scritps/EnemyMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour {
+
+    private Animator _animator;
+    private NavMeshAgent _navMeshAgent;
+    private Transform _player;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        _navMeshAgent.SetDestination(_player.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _animator.SetBool("Attack", true);
+            Debug.Log("DAAAAAAA");
+        }
+    }
+}
